0e88cd0e4860a1e8ca8392a1e7c97554915fd83e
Miguel Angel Vázquez Vázquez <mvk_12@outlook.com>
2020-06-16 19:35:54 -0500

    Corrección de despliegue a github

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-git checkout --orphan gh-pages
+git checkout --orphan master
 mv .vue.config.js vue.config.js
 yarn build
 mv vue.config.js .vue.config.js
 git --work-tree dist add --all
 git --work-tree dist commit -m "Deploy Vue app"
-git push --set-upstream origin gh-pages
-git push origin HEAD:gh-pages --force
+git push --set-upstream origin master
+git push origin HEAD:master --force
 rm -rf dist
-git checkout -f master
-git branch -D gh-pages
+git checkout -f vue-code
+git branch -D master
