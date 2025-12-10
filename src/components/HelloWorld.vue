0ff22d591f7f40bd3a67359eeabf92cbeafad2f9
Miguel Angel Vázquez Vázquez <mvk_12@outlook.com>
2025-12-10 17:34:34 -0600

    feat: migrate to TypeScript
    - Updated App.vue to use TypeScript.
    - Refactored HelloWorld.vue to define props using TypeScript and added a counter feature.
    - Created main.ts to bootstrap the Vue application.
    - Added TypeScript configuration files for app and node.
    - Configured Vite for Vue with TypeScript support.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,17 @@
-<script setup>
-import { ref } from 'vue'
+<script setup lang="ts">
+import { ref } from 'vue';
 
-defineProps({
-  msg: {
-    type: String,
-    required: true,
-  }
-})
+interface Props {
+  msg: string;
+}
+
+defineProps<Props>()
+
+const counter = ref(0)
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
+  <button type="button" @click.prevent="counter++">Click me</button>
+  <p>Clicked {{ counter }} times.</p>
 </template>
